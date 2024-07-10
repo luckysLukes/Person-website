7e4609509e1ebd1addc246e847892da091fd7e1b
Bennett Kosakowski <bekosakowski@wpi.edu>
2024-07-10 14:48:30 -0400

    add js and html

--- 00-intro/intro.js
+++ 00-intro/intro.js
@@ -3,3 +3,21 @@
 //  1. Give every variable/const smallest scope
 //  2. Grab HTML elements by tag and store in const
 //  3. Put all other code inside functions
+
+const colorField = document.getElementById("color-field")
+const fontSizeField = document.getElementById("font-size-field")
+const clickButton = document.getElementById("click-button")
+const text = document.getElementById("text")
+const body = document.body
+
+function buttonPress(){
+    text.innerHTML = "Wow!"
+
+    body.style.backgroundColor = colorField.value
+    text.style.fontSize = `${fontSizeField.value}px`
+    let fail = `${fontSizeField.value}px`
+
+
+
+
+}
