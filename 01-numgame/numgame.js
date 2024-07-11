b91d5a2c2d56d4d28db217a53d73e5ab8931c214
Bennett Kosakowski <bekosakowski@wpi.edu>
2024-07-11 14:54:06 -0400

    V5

--- 01-numgame/numgame.js
+++ 01-numgame/numgame.js
@@ -3,8 +3,52 @@
 //  2. Grab HTML elements by tag and store in const
 //  3. Put all other code inside functions
 
+const numField = document.getElementById("num-field")
+const messageText = document.getElementById("message-text")
+let secret
+let min = 1
+let max = 100
+
 var myConfetti = confetti.create(null, {
     resize: true,
     useWorker: true
 });
 
+function loadGame(){
+    numField.min = min
+    numField.max = max
+    numField.value = max
+    secret = Math.random()
+    secret = secret * (max - min + 1)
+    secret = secret + min
+    secret = Math.floor(secret)
+}
+
+function reloadGame(){
+    numField.min = min
+    numField.max = max
+    numField.value = max
+    secret = Math.random()
+    secret = secret * (max - min + 1)
+    secret = secret + min
+    secret = Math.floor(secret)
+}
+
+
+
+function makeGuess(){
+    let guess = parseInt(numField.value)
+    console.log(`Guess: ${guess}`)
+    if (guess < secret) {
+        messageText.innerHTML = `${guess} is too low`
+    } else if (guess > secret) {
+        messageText.innerHTML = `${guess} is too high`
+    } else if (guess == secret) {
+
+        messageText.innerHTML = `${guess} is correct`
+        myConfetti({
+            particCount: 100,
+            spread: 300
+        })
+    }
+}
